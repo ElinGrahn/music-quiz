0f72e2cb80fe01a7cf5b6888e8cbdfe5c4c5b44c
ElinGrahn <>
2023-10-19 09:09:17 +0000

    add options button, add questions in js

---
+++ assets/javascript/questions.js
@@ -0,0 +1,122 @@
+const questions = [
+    {
+        question: "Which British rock/metal band has recently gotten very popular on platforms like TikTok?",
+        answer: [
+            {text: "Sleep Token", correct: true},
+            { text: "Bad Omens", correct: false },
+            { text: "Spiritbox", correct: false }
+        ]
+    },
+    {
+        question: "Who did the heartfelt song on the soundtrack to the Barbie movie?",
+        answer: [
+            { text: "Taylor Swift", correct: false },
+            { text: "Billie Eilish", correct: true },
+            { text: "Doja Cat", correct: false }
+        ]
+    },
+    {
+        question: "Olivia Rodrigos heartbreak song is titled with what fantasy creature?",
+        answer: [
+            { text: "Vampire", correct: true },
+            { text: "Werewolf", correct: false },
+            { text: "Elf", correct: false }
+        ]
+    },
+    {
+        question: "The band Ghost recently got a lot of new fans for one of thier songs, which one?",
+        answer: [
+            { text: "Cirice", correct: false },
+            { text: "Square Hammer", correct: false },
+            { text: "Mary on a cross", correct: true }
+        ]
+    },
+    {
+        question: "What dance is the Finnish Eurovision song associated to?",
+        answer: [
+            { text: "Samba", correct: false },
+            { text: "Chacha", correct: true },
+            { text: "Salsa", correct: false }
+        ]
+    },
+    {
+        question: "The fanbase dresses up as the artist at their concert, also known as Mr. Worldwide, who is the artist?",
+        answer: [
+            { text: "Pitbull", correct: true },
+            { text: "Chris Brown", correct: false },
+            { text: "Drake", correct: false }
+        ]
+    },
+    {
+        question: "Who sang about music in a sushi resturant?",
+        answer: [
+            { text: "Zayn", correct: false },
+            { text: "Niall Horan", correct: false },
+            { text: "Harry Styles", correct: true }
+        ]
+    },
+    {
+        question: "Which American singer was unexpectedly seen working in a waffle house?",
+        answer: [
+            { text: "Meghan Trainor", correct: false },
+            { text: "Lana Del Rey", correct: true },
+            { text: "Selena Gomez", correct: false }
+        ]
+    },
+    {
+        question: "From which popular tv-show was the song 'Chrissy, Wake Up' born from?",
+        answer: [
+            { text: "Stranger Things", correct: true },
+            { text: "Peaky Blinders", correct: false },
+            { text: "American Horror Story", correct: false }
+        ]
+    },
+    {
+        question: "'It must be exhausting always rooting for the anti-hero' is sung by who?",
+        answer: [
+            { text: "Taylor Swift", correct: false },
+            { text: "Billie Eilish", correct: true },
+            { text: "Rihanna", correct: false }
+        ]
+    },
+    {
+        question: "The show 'The Idol' features which popular artist?",
+        answer: [
+            { text: "Justin Bieber", correct: false },
+            { text: "Frank Ocean", correct: false },
+            { text: "The Weeknd", correct: true }
+        ]
+    },
+    {
+        question: "Which Justin told you to 'go love yourself'?",
+        answer: [
+            { text: "Justin Bieber", correct: true },
+            { text: "Justin Timberlake", correct: false },
+            { text: "Justin Trudeau", correct: false }
+        ]
+    },
+    {
+        question: "Coldplay collaborated with which kpop group?",
+        answer: [
+            { text: "BlackPink", correct: false },
+            { text: "BTS", correct: true },
+            { text: "Exo", correct: false }
+        ]
+    },
+    {
+        question: "Previous Disney Channel star who 'used to be young' and can by herself flowers?",
+        answer: [
+            { text: "Demi Lovato", correct: false },
+            { text: "Selena Gomez", correct: true },
+            { text: "Miley Cyrus", correct: false }
+        ]
+    },
+    {
+        question: "A subgenre of hip hop and trap that got popular on TikTok some year ago?",
+        answer: [
+            { text: "Phonk", correct: false },
+            { text: "Rave", correct: true },
+            { text: "Darkbeat", correct: false }
+        ]
+    },
+]
