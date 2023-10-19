1255e4054aa7e45b6965f30538a481f2244db7b4
ElinGrahn <>
2023-10-19 16:37:25 +0000

    figured out I have to add . before a class...

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,15 @@
 let intro = document.getElementById('intro');
 const answerButtons = document.getElementsByClassName('answer-btn');
-const startButton = document.getElementsByClassName('start-btn');
+const startButton = document.querySelector('.start-btn');
 const nextButton = document.getElementsByClassName('next-btn');
 const SubmitButton = document.getElementsByClassName('submit-btn');
 
 let currentQuestionIndex = 0;
 let score = 0;
+
+startButton.addEventListener('click', startGame);
+
+function startGame (){
+    console.log('hello');
+    startButton.classList.remove('.startButton');
+}
