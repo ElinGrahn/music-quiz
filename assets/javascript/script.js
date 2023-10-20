b8d03cba1ae6d373b1db105633450736d67008fb
ElinGrahn <>
2023-10-20 15:09:18 +0000

    questions show in random order, answer buttons show

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,6 +4,7 @@ const startButton = document.querySelector('.start-btn');
 const nextButton = document.querySelector('.next-btn');
 const SubmitButton = document.querySelector('.submit-btn');
 const restart = document.querySelector('.restart-btn');
+const options = document.querySelector('options-btn');
 
 
 let currentQuestionIndex = 1;
@@ -23,7 +24,19 @@ function nextQuestion() {
 
     let currentQuestion = questions[currentQuestionIndex];
     let questionNum = currentQuestionIndex + 1;
-    intro.innerHTML = currentQuestionIndex + '. ' + currentQuestion.question;
+    intro.innerHTML = currentQuestionIndex + ' out of 15' + "<br>" + currentQuestion.question;
+
+    showActualQuestions();
+    currentQuestion.answer.forEach(answer =>{
+        const button = document.createElement('button');
+        button.innerHTML = answer.text;
+        button.classList.add('options-btn');
+        answerButtons.appendChild(button);
+    })
+}
+
+function showActualQuestions(){
+
 }
 
 answerButtons.addEventListener('click', chooseAnswer);
