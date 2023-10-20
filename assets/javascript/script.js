ae90be711b2e09ff5bb06735b93298452def3c96
ElinGrahn <>
2023-10-20 13:19:56 +0000

    add empty functions and event listeners

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,9 @@
 let intro = document.getElementById('intro');
-const answerButtons = document.getElementsByClassName('answer-btn');
+const answerButtons = document.getElementsByClassName('.answer-btn');
 const startButton = document.querySelector('.start-btn');
-const nextButton = document.getElementsByClassName('next-btn');
-const SubmitButton = document.getElementsByClassName('submit-btn');
+const nextButton = document.getElementsByClassName('.next-btn');
+const SubmitButton = document.getElementsByClassName('.submit-btn');
+const restart = document.getElementsByClassName('.restart-btn');
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -11,5 +12,30 @@ startButton.addEventListener('click', startGame);
 
 function startGame (){
     console.log('hello');
-    startButton.classList.remove('.startButton');
+    intro.remove();
+}
+
+
+answerButtons.addEventListener('click', chooseAnswer);
+
+function chooseAnswer(){
+
+}
+
+nextButton.addEventListener('click', nextQuestion);
+
+function nextQuestion(){
+
+}
+
+SubmitButton.addEventListener('click', submitAnswer);
+
+function submitAnswer(){
+
+}
+
+restart.addEventListener('click', restartGame);
+
+function restartGame(){
+
 }
