88c01fddffe1deb7c202cb25eb1e359262ceec2f
ElinGrahn <>
2023-10-24 16:05:59 +0000

    questions are now random order and game can restart

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -17,18 +17,24 @@ function startGame() {
     answerButtons.classList.remove('hide');
     //Questions will show in a random order
     randomQuestions();
-    showQuestion(); 
+    showQuestion();
 }
-//stack overflow if it works
-function randomQuestions(){
-    let x = questions[Math.floor(Math.random()*questions.length)];
+//w3schools thank you
+function randomQuestions() {
+    let x = questions;
+    for(let i = x.length -1; i > 0; i--){
+        let j = Math.floor(Math.random() * (i+1));
+        let k = x[i];
+        x[i] = x[j];
+        x[j] = k;
+    }
 }
 
 //Display the questions
 function showQuestion() {
     let currentQuestion = questions[currentQuestionIndex];
     let questionNum = currentQuestionIndex + 1;
-    intro.innerHTML = questionNum + ' out of 15' + "<br>" + currentQuestion.question;
+    intro.innerHTML = questionNum + ' out of 16' + "<br>" + currentQuestion.question;
 
 
     showActualQuestions();
@@ -37,7 +43,7 @@ function showQuestion() {
         button.innerHTML = answer.text;
         button.classList.add('options-btn');
         answerButtons.appendChild(button);
-        if(answer.correct){
+        if (answer.correct) {
             answerButtons.dataset.correct = answer.correct;
         }
         answerButtons.addEventListener('click', selectAnswer);
@@ -61,32 +67,44 @@ function submitAnswer() {
 
 nextButton.addEventListener('click', nextQuestion);
 
-function nextQuestion(){
-    for(let i = 0; i <= questions.length; i++){
-        if(i > questions.length){
-            restartGame();
-        }
+function nextQuestion() {
+    if (currentQuestionIndex < questions.length) {
+        console.log(currentQuestionIndex);
+    } else {
+        endOfGame();
     }
+
     showQuestion();
     currentQuestionIndex++;
-    startGame();
     submitButton.classList.add('hide');
     nextButton.classList.add('hide');
 }
 
-restart.addEventListener('click', restartGame);
+restart.addEventListener('click', startOver);
 
-function restartGame() {
-    console.log('good');
+function startOver(){
+    startButton.classList.remove('hide');
+    restart.classList.add('hide');
+    currentQuestionIndex = 0;
+    intro.innerHTML = questionNum + ' out of 16' + "<br>" + currentQuestion.question;
 }
 
-function selectAnswer(event){
+function endOfGame() {
+    console.log('good');
+    intro.innerHTML = `Good job! Your score is <br> ${currentQuestionIndex} out of 16.<br> Would you like to try again?`;
+    answerButtons.classList.add('hide');
+    nextButton.classList.add('hide');
+    submitButton.classList.add('hide');
+    restart.classList.remove('hide');
+}
+
+function selectAnswer(event) {
     let chosenAnswer = event.target;
     let correctAnswer = chosenAnswer.dataset.correct;
-    if(correctAnswer){
+    if (correctAnswer) {
         chosenAnswer.classList.add('correct');
         score++;
-    } else{
+    } else {
         chosenAnswer.classList.add('incorrect');
     }
     submitButton.classList.remove('hide');
